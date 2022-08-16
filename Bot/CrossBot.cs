06df881c251999e3589d4fd157c24bba46dcd3b4
berichan <66521620+berichan@users.noreply.github.com>
2022-08-16 20:03:56 +0100

    Reset files on restore requested

--- Bot/CrossBot.cs
+++ Bot/CrossBot.cs
@@ -213,7 +213,7 @@ namespace SysBot.ACNHOrders
                     {
                         RestoreRestartRequested = false;
                         await ResetFiles(token).ConfigureAwait(false);
-                        await AttemptEchoHook($"[{DateTime.Now:yyyy-MM-dd hh:mm:ss tt}] Restart requested. Please wait for the new dodo code for {TownName}.", Config.DodoModeConfig.EchoDodoChannels, token).ConfigureAwait(false);
+                        await AttemptEchoHook($"[{DateTime.Now:yyyy-MM-dd hh:mm:ss tt}] Please wait for the new dodo code for {TownName}.", Config.DodoModeConfig.EchoDodoChannels, token).ConfigureAwait(false);
                         await DodoRestoreLoop(true, token).ConfigureAwait(false);
                         return;
                     }
@@ -1148,6 +1148,7 @@ namespace SysBot.ACNHOrders
         private async Task ResetFiles(CancellationToken token)
         {
             string DodoDetails = Config.DodoModeConfig.MinimizeDetails ? "FETCHING" : $"{TownName}: FETCHING";
+            DodoCode = DodoDetails;
             byte[] encodedText = Encoding.ASCII.GetBytes(DodoDetails);
             await FileUtil.WriteBytesToFileAsync(encodedText, Config.DodoModeConfig.DodoRestoreFilename, token).ConfigureAwait(false);
 
