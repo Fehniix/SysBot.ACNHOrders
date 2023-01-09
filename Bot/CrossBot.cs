491438f607ccd2ee612e2b5bd4fae8caf91a60c1
DESKTOP-SN9KF5O\Strawberry <berichandev@gmail.com>
2023-01-09 23:31:15 +0000

    trycatch whole function loops

--- Bot/CrossBot.cs
+++ Bot/CrossBot.cs
@@ -173,12 +173,28 @@ namespace SysBot.ACNHOrders
                 }
 
                 LogUtil.LogInfo("Orders not accepted in dodo restore mode! Please ensure all joy-cons and controllers are docked! Starting dodo restore loop...", Config.IP);
-                while (!token.IsCancellationRequested)
-                    await DodoRestoreLoop(false, token).ConfigureAwait(false);
+                try
+                {
+                    while (!token.IsCancellationRequested)
+                        await DodoRestoreLoop(false, token).ConfigureAwait(false);
+                }
+                catch (Exception e) 
+                {
+                    LogUtil.LogError($"Dodo restore loop ended with error: {e.Message}\r\n{e.StackTrace}", Config.IP);
+                    return;
+                }
             }
 
-            while (!token.IsCancellationRequested)
-                await OrderLoop(token).ConfigureAwait(false);
+            try
+            { 
+                while (!token.IsCancellationRequested)
+                    await OrderLoop(token).ConfigureAwait(false);
+            }
+            catch (Exception e)
+            {
+                LogUtil.LogError($"Order loop ended with error: {e.Message}\r\n{e.StackTrace}", Config.IP);
+                return;
+            }
         }
 
         private async Task DodoRestoreLoop(bool immediateRestart, CancellationToken token)
